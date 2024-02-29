df91a4b9e0cd3e530863b62637ae3bc2859501c3
xRenanMartins <renan-martins955@hotmail.com>
2024-02-29 17:12:36 -0300

    tailwind css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -14,9 +15,29 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: colors.indigo,
+                secondary: {
+                    100: "#cfd1d4",
+                    200: "#a0a3a9",
+                    300: "#70747d",
+                    400: "#414652",
+                    500: "#111827",
+                    600: "#0e131f",
+                    700: "#0a0e17",
+                    800: "#070a10",
+                    900: "#030508"
+                },
+            }
         },
+    },
+    variants: {
+        extends: {
+            opacity: ['disabled'],
+            scale: ['active']
+        }
     },
 
     plugins: [forms, typography],
